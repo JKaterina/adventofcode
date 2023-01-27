1d81bd22d95aea44686d9a022090df9733f7e270
JKaterina <uljanova.jekaterina@gmail.com>
2023-01-27 17:30:53 +0100

    solved task 2 of challenge 1

--- challenge1.py
+++ challenge1.py
@@ -2,15 +2,26 @@
 with open("inputs/input_ch1.txt") as f:
     input1 = list(f)
 
+### DATA CLEANING ###
+
 # remove '\n' --> empty string is a batch separator
 a = [s.strip('\n') for s in input1]
 
 # transform string to digits
 b = [int(s) if s.isdigit() else '' for s in a]
 
-# roll over the list until the next separator and sum digits in each batch
+# split the list into sublists based on empty string separator
 from itertools import groupby
 res = [list(sub) for ele, sub in groupby(b, key = bool) if ele]
-        
+
+### CHALLENGE 1: FIND THE HIGHEST SUM ###
+
+# sum all elements in each sublist and find the highest value
 all_sums = [sum(a_list) for a_list in res]
 print(max(all_sums))
+
+### CHALLENGE 2: FIND THE TOP THREE AND CALCULATE THEIR SUM ###
+
+# sort the list of all sums and take the top 3
+top_sums = sorted(all_sums, reverse = True)
+print(sum(top_sums[:3]))
