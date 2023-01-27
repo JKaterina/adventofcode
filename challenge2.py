12ea6a8e9fb79b8a22ce4ac26a77a9fb25d92e6c
JKaterina <uljanova.jekaterina@gmail.com>
2023-01-27 20:03:45 +0100

    solved part 1

--- challenge2.py
+++ challenge2.py
@@ -1,10 +1,12 @@
 # load the file as pandas dataframe
 
 import pandas as pd
-data = pd.read_csv("inputs/input_ch2.txt", names = ['Player 1', 'Player 2'])
+data = pd.read_csv("inputs/input_ch2.txt", sep = ' ', names = ['Player 1', 'Player 2'])
 
-data['Player 2'] = data['Player 1'].apply(lambda x: x[1:])
-data['Player 1'] = data['Player 1'].apply(lambda x: x[:1])
+print(data.head())
+
+# data['Player 2'] = data['Player 1'].apply(lambda x: x[1:]).astype('str')
+# data['Player 1'] = data['Player 1'].apply(lambda x: x[:1])
 
 ### DECISION RULE FOR THE WINNING PARTY ###
 
@@ -24,7 +26,7 @@ data['Player 1'] = data['Player 1'].apply(lambda x: x[:1])
 # replace letters with digits
 a_dict = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
 
-#data['p1 score'] = data['Player 1'].map(a_dict)
+data['p1 score'] = data['Player 1'].map(a_dict)
 data['p2 score'] = data['Player 2'].map(a_dict)
 
 print(data.head())
@@ -32,7 +34,13 @@ print(data.head())
 # identify the winner & assign score per round
 winner_dict = {0: 3, -1: 0, 1: 6, -2: 6, 2: 0}
 
-# assign the score per round
+data['p2 score per round'] = data['p2 score'] - data['p1 score']
 
-# calculate the sum per round
+print(data.head())
+
+# assign the score per round
+data['score translation'] = data['p2 score per round'].map(winner_dict)
+# calculate Player 2 sum per round
+data['p2 final score'] = data['p2 score'] + data['score translation']
 # calculate the final score
+print(data['p2 final score'].sum())
