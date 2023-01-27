1e0906ff0d27f21e35422331443d413fc87474e5
JKaterina <uljanova.jekaterina@gmail.com>
2023-01-27 19:47:54 +0100

    changed data structure to dataframe, defined decision rule

--- challenge2.py
+++ challenge2.py
@@ -1,4 +1,38 @@
-# load the file as list
-with open("inputs/input_ch2.txt") as f:
-    input2 = list(f)
-    print(input2[:3])
+# load the file as pandas dataframe
+
+import pandas as pd
+data = pd.read_csv("inputs/input_ch2.txt", names = ['Player 1', 'Player 2'])
+
+data['Player 2'] = data['Player 1'].apply(lambda x: x[1:])
+data['Player 1'] = data['Player 1'].apply(lambda x: x[:1])
+
+### DECISION RULE FOR THE WINNING PARTY ###
+
+# A OR X == Rock 1-1 = 0 draw 1-2 = -1 loss 1-3 = -2 win 
+# B OR Y == Paper 2-2 = 0 draw 2-3 = -1 loss 2-1 = 1 win 
+# C OR Z == Scissors 3-3 = 0 draw 3-1 = 2 loss 3-2 = 1 win 
+
+# decision rule:
+# 0 always draw
+# -1 always loss
+# 1 always win
+# -2 win
+# 2 loss
+
+# score allocation: 0 (loss), 3 (draw), 6 (win)
+
+# replace letters with digits
+a_dict = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
+
+#data['p1 score'] = data['Player 1'].map(a_dict)
+data['p2 score'] = data['Player 2'].map(a_dict)
+
+print(data.head())
+
+# identify the winner & assign score per round
+winner_dict = {0: 3, -1: 0, 1: 6, -2: 6, 2: 0}
+
+# assign the score per round
+
+# calculate the sum per round
+# calculate the final score
